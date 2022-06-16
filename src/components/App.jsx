4616670c102b36e838bdafd5b43d7f81f7e9db72
aleksandr105 <aleksandrsherbina105@gmail.com>
2022-06-16 23:42:14 +0200

    Add render profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,13 @@
+import { Profile } from 'components/Profile/Profile';
+import user from 'user.json';
+// import PropTypes from 'prop-types';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Profile key={user.username} avatar={user.avatar} username={user.username} tag={user.tag} location={user.location}
+        followers={user.stats.followers} views={user.stats.views} likes={user.stats.likes}/>
     </div>
   );
 };
+
